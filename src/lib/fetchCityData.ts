b4529eafd333e39e1be494ea1d92e5b49234fa5b
remilebeau <remilebeau90@gmail.com>
2024-08-14 00:55:21 -0400

    fix fetchCityData for production

--- src/lib/fetchCityData.ts
+++ src/lib/fetchCityData.ts
@@ -11,9 +11,16 @@ export default async function fetchCityData(
   city: string,
   state: string
 ): Promise<CityData> {
-  const response = await fetch(
-    `http://localhost:8000/api/citydata?city=${city}&state=${state}`
-  );
+  const DATA_URL =
+    process.env.NODE_ENV === "production"
+      ? `https://citydata-api.onrender.com/api/citydata?city=${city}&state=${state}`
+      : `http://localhost:8000/api/citydata?city=${city}&state=${state}`;
+  const response = await fetch(DATA_URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   const data = await response.json();
   return data;
